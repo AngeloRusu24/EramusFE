e70d6979528377ae7eddb8f7da8f63277e2a1fe9
EliasRaccoon <angelorusu24@gmail.com>
2026-05-03 13:44:21 +0200

    spostato cartelle

---
+++ .next/static/chunks/app/layout.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Fhome%2Felias%2FGitHub%2FEramus%2FEramusFE%2Fnode_modules%2Fbootstrap-italia%2Fdist%2Fcss%2Fbootstrap-italia.min.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!":
+/*!***************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Fhome%2Felias%2FGitHub%2FEramus%2FEramusFE%2Fnode_modules%2Fbootstrap-italia%2Fdist%2Fcss%2Fbootstrap-italia.min.css%22%2C%22ids%22%3A%5B%5D%7D&server=false! ***!
+  \***************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./node_modules/bootstrap-italia/dist/css/bootstrap-italia.min.css */ \"(app-pages-browser)/./node_modules/bootstrap-italia/dist/css/bootstrap-italia.min.css\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyJTJGaG9tZSUyRmVsaWFzJTJGR2l0SHViJTJGRXJhbXVzJTJGRXJhbXVzRkUlMkZub2RlX21vZHVsZXMlMkZib290c3RyYXAtaXRhbGlhJTJGZGlzdCUyRmNzcyUyRmJvb3RzdHJhcC1pdGFsaWEubWluLmNzcyUyMiUyQyUyMmlkcyUyMiUzQSU1QiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLDRQQUF1SSIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzYwYzEiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCIvaG9tZS9lbGlhcy9HaXRIdWIvRXJhbXVzL0VyYW11c0ZFL25vZGVfbW9kdWxlcy9ib290c3RyYXAtaXRhbGlhL2Rpc3QvY3NzL2Jvb3RzdHJhcC1pdGFsaWEubWluLmNzc1wiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Fhome%2Felias%2FGitHub%2FEramus%2FEramusFE%2Fnode_modules%2Fbootstrap-italia%2Fdist%2Fcss%2Fbootstrap-italia.min.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./node_modules/bootstrap-italia/dist/css/bootstrap-italia.min.css":
+/*!*************************************************************************!*\
+  !*** ./node_modules/bootstrap-italia/dist/css/bootstrap-italia.min.css ***!
+  \*************************************************************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"7072e3b0f1e3\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9ib290c3RyYXAtaXRhbGlhL2Rpc3QvY3NzL2Jvb3RzdHJhcC1pdGFsaWEubWluLmNzcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vbm9kZV9tb2R1bGVzL2Jvb3RzdHJhcC1pdGFsaWEvZGlzdC9jc3MvYm9vdHN0cmFwLWl0YWxpYS5taW4uY3NzP2Y2ZjgiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCI3MDcyZTNiMGYxZTNcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/bootstrap-italia/dist/css/bootstrap-italia.min.css\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Fhome%2Felias%2FGitHub%2FEramus%2FEramusFE%2Fnode_modules%2Fbootstrap-italia%2Fdist%2Fcss%2Fbootstrap-italia.min.css%22%2C%22ids%22%3A%5B%5D%7D&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
